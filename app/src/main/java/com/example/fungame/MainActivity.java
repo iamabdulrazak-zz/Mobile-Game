8f4d7164abe63fdef960826ba4aa59147c9a7044
Abdulrazak Osman <zaak0101@hotmail.com>
2020-01-14 16:12:37 +0300

    first app using webview

---
+++ app/src/main/java/com/example/fungame/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.fungame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://fungame.azurewebsites.net/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+        super.onBackPressed();
+        }
+    }
+}
